b0318fc690c71a31f3f80c4dd15c66cff7037d73
Ozasnidev <ozasnidev@gmail.com>
2021-02-08 13:35:26 +0100

    02 - Sass installation & setting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MyDayApp } from './components/App/MyDayApp';
 
+import './styles/styles.scss'
+
 ReactDOM.render(
     <MyDayApp />,
   document.getElementById('root')
