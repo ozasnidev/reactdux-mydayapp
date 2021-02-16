4d317f09cc26bfa761a857cbc7460ba9a2a9088c
Ozasnidev <ozasnidev@gmail.com>
2021-02-16 19:18:19 +0100

    19 - Dispatching auth action to store

--- src/components/auth/login/LoginForm.js
+++ src/components/auth/login/LoginForm.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import { useDispatch } from "react-redux";
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useForm } from '../../../hooks/useForm';
+import { loginAction } from '../actions/authActions';
 
 export const LoginForm = () => {
+    const dispatch = useDispatch();
+    const [ values, handleInputChange] = useForm({
+        email:'Juan@juan.com',
+        password:'123456'
+    })
+    const{ email, password } = values
+
+    const handleLogin = event => {
+        event.preventDefault()
+        dispatch( loginAction(98765, 'Ozasnidev') )
+    }
+
     return (
         <>
-            <form>
-                <input type="text" placeholder="email" name="email" className="login__input mx-3" autoComplete="off"/>
-                <input type="password" placeholder="password" name="password" className="login__input mx-3" autoComplete="off"/>
+            <form onSubmit={ handleLogin }>
+                <input type="text" placeholder="email" name="email" className="login__input mx-3" autoComplete="off" value={email} onChange={handleInputChange}/>
+                <input type="password" placeholder="password" name="password" className="login__input mx-3" autoComplete="off" value={password} onChange={handleInputChange}/>
                 <button type="submit" className="mx-10 p-12" >Sign in</button>
                 <button className="login__google">
                     <FontAwesomeIcon icon={['fab', 'google']} size="2x"/>
--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers } from "redux";
 import { authReducer } from "../components/auth/reducers/authReducer"
 
 const combinedReducers = combineReducers({
-    authReducer
+    authReducer : authReducer
 })
 
 export const reduxStore = createStore(combinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
