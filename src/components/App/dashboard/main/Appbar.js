0ab3f41f47111418a5e6c06ae4ce846b7f3f35ad
Ozasnidev <ozasnidev@gmail.com>
2021-02-16 13:35:59 +0100

    15 - Entry section styles

--- src/components/App/dashboard/main/Appbar.js
+++ src/components/App/dashboard/main/Appbar.js
@@ -5,8 +5,8 @@ export const Appbar = () => {
         <div className="dashboard__entry-section-appbar mt-8 mr-8">
             <h2>08 de Abril 2021</h2>
             <div className="dashboard__entry-section-appbar-buttons">
-                <button>Photo</button>
-                <button>Save</button>
+                <button className="m-10">Photo</button>
+                <button className="m-10">Save</button>
             </div>
         </div>
     )
--- src/components/App/dashboard/main/EntrySection.js
+++ src/components/App/dashboard/main/EntrySection.js
@@ -1,4 +1,5 @@
 //NoteScreen
+//NoteScreen
 import React from 'react'
 import { Appbar } from './Appbar'
 
@@ -6,6 +7,24 @@ export const EntrySection = () => {
     return (
         <div className="dashboard__entry-section-content"> 
             <Appbar />
+            <div className="dashboard__entry-section-data-content">
+                <input 
+                    type="text"
+                    placeholder="Amazing experience name"
+                    className="dashboard__entry-section-title-input mb-5"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="Tell us your experience with details!"
+                    className="dashboard__entry-section-textarea mb-5 p-5"
+                ></textarea>
+                <div className="dashboard__entry-section-image">
+                    <img 
+                        src="https://skyticket.com/guide/wp-content/uploads/2019/09/shutterstock_1135776710-680x453.jpg"
+                        alt="ancient Japan"    
+                    />
+                </div>
+            </div>
         </div>
     )
 }
