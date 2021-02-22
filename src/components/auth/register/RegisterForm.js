953867e233b993aea4a90e8661005bfb247c1236
Ozasnidev <ozasnidev@gmail.com>
2021-02-22 18:33:31 +0100

    22 - User form register validation

--- src/components/auth/register/RegisterForm.js
+++ src/components/auth/register/RegisterForm.js
@@ -12,12 +12,34 @@ export const RegisterForm = () => {
     const { name, email, password, password2 } = formData
     const handleRegister = (event) => {
         event.preventDefault()
+        if(isUserDataValid()){
+            console.log("Formulario correcto")
+        }
 
+    }
+    const isUserDataValid = () => {
+        const numInUserNameRegex = /[0-9]/
+        const emailRegexPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if( name.trim().length === 0 || numInUserNameRegex.test(name)){
+            console.log("Error");
+            return false;
+        }else if(!emailRegexPattern.test(email)){
+            console.log("Invalid email");
+            return false;
+        }else if( password !== password2){
+            console.log("Passwords must to match each other");
+            return false;
+        }else if(password.length < 6 && password2.length < 6){
+            console.log("Passwords must at least to have 6 characters")
+            return false
+        }
+        return true
     }
 
     return (
         <>
             <form onSubmit={handleRegister}>
+                <div className="alert__error m-10"> Hola Mundo </div>
                 <input type="text" placeholder="name" name="name" className="login__input mx-3" autoComplete="off" value={name} onChange={handleInputChange}/>
                 <input type="text" placeholder="email" name="email" className="login__input mx-3" autoComplete="off" value={email} onChange={handleInputChange}/>
                 <input type="password" placeholder="password" name="password" className="login__input mx-3" autoComplete="off" value={password} onChange={handleInputChange}/>
